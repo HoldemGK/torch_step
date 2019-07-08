cb0833895c99f0890621322163464dba963a0c0d
Sanchez <skrolsky@gmail.com>
2019-07-08 18:21:34 +0300

    regress prerrel

--- regress.py
+++ regress.py
@@ -3,12 +3,12 @@ import torch
 class RegressionNet(torch.nn.Module):
     def __init__(self, n_hiden_neurons):
         super(RegressionNet, self).__init__()
-        NUMNE = n_hiden_neurons
-        self.fc1 = torch.nn.Linear(1, NUMNE)
+        NUM = n_hiden_neurons
+        self.fc1 = torch.nn.Linear(1, NUM)
         self.act1 = torch.nn.Tanh()
-        self.fc2 = torch.nn.Linear(NUMNE, NUMNE)
+        self.fc2 = torch.nn.Linear(NUM, NUM)
         self.act2 = torch.nn.Tanh()
-        self.fc3 = torch.nn.Linear(NUMNE, 1)
+        self.fc3 = torch.nn.Linear(NUM, 1)
 
     def forward(self, x):
         x = self.fc1(x)
@@ -43,4 +43,14 @@ def metric(pred, target):
 optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
 
 def loss(pred, target):
-    return (pred - target).abs().mean()
+    squares = (pred - target) ** 2
+    return squares.mean()
+
+for epoch_index in range(2000):
+    optimizer.zero_grad()
+    y_pred = net.forward(x_train)
+    loss_value = loss(y_pred, y_train)
+    loss_value.backward()
+    optimizer.step()
+
+print(metric(net.forward(x_validation), y_validation).item())
